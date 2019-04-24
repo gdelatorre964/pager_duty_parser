248da70f661c54d92c1edb43b4baa5557cac154a
Admin <gdelatorre964@gmail.com>
2019-04-24 08:19:41 -0500

    Latest version 4/24/19

--- pager_duty.py
+++ pager_duty.py
@@ -1,30 +1,6 @@
 #!/usr/bin/env python
 #
 # Copyright (c) 2016, PagerDuty, Inc. <info@pagerduty.com>
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#     * Redistributions of source code must retain the above copyright
-#       notice, this list of conditions and the following disclaimer.
-#     * Redistributions in binary form must reproduce the above copyright
-#       notice, this list of conditions and the following disclaimer in the
-#       documentation and/or other materials provided with the distribution.
-#     * Neither the name of PagerDuty Inc nor the
-#       names of its contributors may be used to endorse or promote products
-#       derived from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL PAGERDUTY INC BE LIABLE FOR ANY
-# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
 # Sample script to output incident details to a CSV in the format:
 # incident_id,created_at,type,user_or_agent_id,user_or_agent_summary,notification_type,channel_type,summary
 # CLI Usage: ./get_incident_details_csv api_key [since] [until]
@@ -33,17 +9,30 @@
 #   until: End date of incidents you want to pull in YYYY-MM-DD format
 
 import csv
+import re
 import sys
 from datetime import date
+from datetime import datetime
 
 import requests
-from urllib3.connectionpool import xrange
+
+
+def clean_notes(note):
+    # will receive the entire note separated by commas, remove spaces and assign properly
+    note = note.split(':')
+    try:
+        note = note[1].split(',')
+        issue = (note[0]).strip(); solution = (note[1]).strip(); tag = ((note[2]).strip()).upper()
+
+    except:
+        note = note[0].split(',')
+        issue = (note[0]).strip(); solution = (note[1]).strip(); tag = ((note[2]).strip()).upper()
+    return issue,solution,tag
 
 
 def get_incident_count(since, until, headers):
     # Retrieve the count of incidents in a given time-period, as an 'int'.
     # Dates should be in the format 'YYYY-MM-DD'.
-    print("Getting incidents count")
     payload = {
         'since': since,
         'until': until
@@ -53,107 +42,87 @@ def get_incident_count(since, until, headers):
     return int(r.json()['total'])
 
 
-def get_incident_ids(since, until, headers, ea_hun=None):
+def get_incident_ids(since, until, headers):
     # Based on an incident-count used to create an 'offset',
     # retrieve incident-IDs, in batches-of-100, and return as a list.
     # Dates should be in the format 'YYYY-MM-DD'.
-    print("Getting incident ids")
     id_list = []
     count = get_incident_count(since, until, headers)
-    get_incident_ids_url = 'https://api.pagerduty.com/incidents'
+    print('Number of incidents since ', since, ' to ', until, 'is', count)
+    get_incident_ids_url = 'https://api.pagerduty.com/incidents?'
     payload = {'since': since, 'until': until}
-    for ea_hun in xrange(0, count) or int(ea_hun) == count:
-        if int(ea_hun) % 100 == 1:
-            payload['offset'] = ea_hun
-            r = requests.get(get_incident_ids_url, params=payload, headers=headers, stream=True)
-            id_list = id_list + [ea_inc['id'] for ea_inc in r.json()['incidents']]
+    r = requests.get(get_incident_ids_url, params=payload, headers=headers, stream=True)
+    id_list = id_list + [ea_inc['id'] for ea_inc in r.json()['incidents']]
     return id_list
 
 
-def get_details_by_incident(api_key, filename='pagerduty_export', since='', until=date.today()):
-    # Based on a list of incident-IDs, retrieve incident details.
-    # Process json-payload and output to CSV file.
-    print("Getting incident details")
+def get_details_by_incident(api_key, since='', until=date.today(), filename='pagerduty_export'):
     headers = {
         'Accept': 'application/vnd.pagerduty+json;version=2',
         'Authorization': 'Token token=' + api_key
     }
     id_list = get_incident_ids(since, until, headers)
-    print("here")
-    fin_file = open('{filename}.csv'.format(filename=filename), 'w')
-    fieldnames = ['Phone call from', 'created_at', 'type', 'user_or_agent_id', 'user_or_agent_summary', 'notification_type',
-                  'channel_type', 'summary']
+    print(id_list)
+    fin_file = open('{filename}.csv'.format(filename=filename), 'w', newline='')
+    fieldnames = ['source', 'create_time', 'tech', 'issue', 'solution', 'tag', 'duration']
     writer = csv.DictWriter(fin_file, fieldnames=fieldnames)
     writer.writeheader()
-    print("after write")
+    regex = r"(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2})"
+    regex1 = r"(^210|830|512|)(\d+)$"
+    FMT = '%Y-%m-%dT%H:%M:%S'
     for ea_id in id_list:
-        print("in for")
-        r = requests.get('https://api.pagerduty.com/incidents'.format(ea_id), headers=headers,
-                         stream=True)
-        print("after request")
+        r = requests.get('https://api.pagerduty.com/incidents/{0}/alerts'.format(ea_id), headers=headers, stream=True)
+        for ea_entry in reversed(r.json()['alerts']):
+            source = ea_entry['body']['details']['Call back']
+            if len(source) > 3:
+                try:
+                    matches = re.match(regex1, source)
+                    source = store_number_dict[matches.group(2)]
+                except:
+                    print("NOT A STORE NUMBER")
+            created_time = re.match(regex, ea_entry['created_at'])
+            try:
+                resolve_time = re.match(regex, ea_entry['resolved_at'])
+                tdelta = (datetime.strptime(resolve_time.group(1), FMT)) - (
+                    datetime.strptime(created_time.group(1), FMT))
+            except:
+                tdelta = "Open Ticket"
 
-        for ea_entry in reversed(r.json()['incidents']):
-            print(ea_entry)
-            """
-            if ea_entry['type'] != 'notify_log_entry_reference' and ea_entry['type'] != 'notify_log_entry':
-                print("1st if")
-                if ea_entry['channel']['type'] == 'nagios' or ea_entry['channel']['type'] == 'web_trigger' or \
-                        ea_entry['channel']['type'] == 'email' or ea_entry['channel']['type'] == 'api':
-                    print("2nd if")
-                    row = {
-                        'Phone call from': ea_id,
-                        'created_at': ea_entry['created_at'],
-                        'type': ea_entry['type'],
-                        'user_or_agent_id': 'N/A',
-                        'user_or_agent_summary': 'N/A',
-                        'notification_type': 'N/A',
-                        'channel_type': ea_entry['channel']['type'],
-                        'summary': ea_entry['summary']
-                    }
-                elif ea_entry['type'] == 'assign_log_entry' or ea_entry['type'] == 'assign_log_entry_reference' or \
-                        ea_entry['type'] == 'acknowledge_log_entry' or ea_entry[
-                    'type'] == 'acknowledge_log_entry_reference' or ea_entry['type'] == 'resolve_log_entry' or ea_entry[
-                    'type'] == 'resolve_log_entry_reference' or ea_entry['type'] == 'snooze_log_entry' or ea_entry[
-                    'type'] == 'snooze_log_entry_reference' or ea_entry['type'] == 'annotate_log_entry' or ea_entry[
-                    'type'] == 'annotate_log_entry_reference':
-                    row = {
-                        'incident_id': ea_id,
-                        'created_at': ea_entry['created_at'],
-                        'type': ea_entry['type'],
-                        'user_or_agent_id': ea_entry['agent']['id'],
-                        'user_or_agent_summary': ea_entry['agent']['summary'],
-                        'notification_type': 'N/A',
-                        'channel_type': ea_entry['channel']['type'],
-                        'summary': ea_entry['summary']
-                    }
-            else:
-                row = {
-                    'incident_id': ea_id,
-                    'created_at': ea_entry['created_at'],
-                    'type': ea_entry['type'],
-                    'user_or_agent_id': ea_entry['user']['id'],
-                    'user_or_agent_summary': ea_entry['user']['summary'],
-                    'notification_type': ea_entry['channel']['notification']['type'].replace('\n', ''),
-                    'channel_type': ea_entry['channel']['type'],
-                    'summary': ea_entry['summary']
-                }
-            writer.writerow(row) """
-    print("Successfully created {filename}.csv!".format(filename=filename))
+            s = requests.get('https://api.pagerduty.com/incidents/{0}/notes'.format(ea_id), headers=headers,
+                             stream=True)
+            for eb_entry in reversed(s.json()['notes']):
+                tech = eb_entry['user']['summary']
+                issue, solution,tag = clean_notes(eb_entry['content'])
+                print(issue, solution,tag)
+            row = {
+                'source': source,
+                'create_time': created_time.group(1),
+                'tech': tech,
+                'issue': issue,
+                'solution': solution,
+                'tag': tag,
+                'duration': tdelta
+            }
+            writer.writerow(row)
+        # print('{0},{1},{2},{3},{4}'.format(source, ea_entry['created_at'], tech, note, tdelta))
     fin_file.close()
 
 
 if __name__ == '__main__':
+    with open('store_numbers.csv', mode='r') as infile:
+        reader = csv.reader(infile)
+        store_number_dict = {rows[1]: rows[0] for rows in reader}
+    with open('tags.csv', mode='r') as infile:
+        reader = csv.reader(infile)
+        tag_dict = {rows[1]: rows[0] for rows in reader}
+    print(tag_dict)
+    with open('tags.csv', mode='r') as infile:
+        reader = csv.reader
     if len(sys.argv) == 1:
         print(
             'Error: You did not enter any parameters.\nUsage: ./get_incident_details_csv api_key [filename] [since] ['
             'until]\n\tapi_key: PagerDuty API access token\n\tfilename: Name of the CSV file. Defaults to '
             'pagerduty_export.\n\tsince: Start date of incidents you want to pull in YYYY-MM-DD format\n\tuntil: End '
             'date of incidents you want to pull in YYYY-MM-DD format')
-    elif len(sys.argv) == 2:
-        get_details_by_incident(sys.argv[1])
-    elif len(sys.argv) == 3:
-        get_details_by_incident(sys.argv[1], sys.argv[2])
-    elif len(sys.argv) == 4:
-        get_details_by_incident(sys.argv[1], sys.argv[2], sys.argv[3])
     else:
-        get_details_by_incident(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+        get_details_by_incident(sys.argv[1], sys.argv[2], sys.argv[3])
